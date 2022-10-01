ce9bf834af175cc780ff12d5d123ae427718fbde
tmrob2 <tmrob2@gmail.com>
2022-10-01 11:23:11 +1000

    fixed a bug with minigrid usage, started building ppo network for POMDP

--- mappo/minigrid_tests.py
+++ mappo/minigrid_tests.py
@@ -1,18 +1,23 @@
 # minigrid works with gym 0.26
 # rware does not support gym 0.26 so this is something to be aware of up front
 
+import os
 
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
+
+from utils import FlatImageGrid
 import minigrid
-import gym
+import gymnasium as gym_ # the gym environment has moved to gymnasium for minigrid
+import numpy as np
 
 # create a simple empty partially observable 8x8 grid
-env = gym.make('MiniGrid-Empty-8x8-v0')
+env = gym_.make('MiniGrid-Empty-8x8-v0')
 
 print("observation space: ", env.observation_space.shape)
 observation, info = env.reset()
 
 # this is the deafault partially observable environment. 
-print("state: ", observation) 
+#print("state: ", observation) 
 
 # Flat observation wrapper will give us a single tensor with the direction 
 # and all objects encoded without the NLP mission string.
@@ -24,6 +29,42 @@ from functools import reduce
 import operator
 
 print(reduce(operator.mul, observation["image"].shape, 1))
+
+print(observation["direction"])
+
+print("flat obs: ", np.append(observation["image"].flatten(), observation["direction"]))
+
+# use a flat image created from utils
+
+env = FlatImageGrid(env)
+
+
+obs_flat, info = env.reset()
+
+print("flat observation", obs_flat)
+obs_shape = env.observation_space.shape[0]
+print("observation space", obs_shape)
+
+# Now with the flat observation we want to input it into a test LSTM layer
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+lstm = nn.LSTM(obs_shape, 256, batch_first=True)
+
+# convert the observation to a tensor
+
+obs_ = torch.tensor(obs_flat, dtype=torch.float32)
+hxs = torch.zeros(1, 256, dtype=torch.float32, device= "cpu")
+cxs = torch.zeros(1, 256, dtype=torch.float32, device="cpu")
+x, (hxs, cxs) = lstm(obs_.unsqueeze(0), (hxs, cxs))
+
+print(x)
+
+
+
+
+
 
 
 
--- mappo/utils.py
+++ mappo/utils.py
@@ -1,5 +1,6 @@
+import numpy as np
 from functools import reduce
-import gym
+import gymnasium as gym_
 from minigrid.wrappers import ObservationWrapper
 import operator
 from functools import reduce
@@ -15,3 +16,16 @@ class FlatImageGrid(ObservationWrapper):
         # although an lstm expects our input shape to be a three dimensional vector. 
         # this is not necessarily bad through because we can use the trick from the POMDP
         # carpole tests
+        self.observation_space = gym_.spaces.Box(
+            low=0,
+            high=255,
+            shape=(imgSize + 1,),
+            dtype="uint8"
+        )
+
+    def observation(self, obs):
+        # flatten the image
+        img_ = obs["image"].flatten()
+        dir_ = obs["direction"]
+        obs = np.append(img_, dir_)
+        return obs
